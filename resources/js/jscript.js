940087285501abcb72802a1cac9558cd0879e57c
SimUI <133499867+Simonceeno@users.noreply.github.com>
2024-08-09 10:52:49 +0200

    prettified css and js

--- resources/js/jscript.js
+++ resources/js/jscript.js
@@ -6,25 +6,25 @@ const firstLine = document.getElementById("setup");
 const secondLine = document.getElementById("punchline");
 
 // API fetching
-const getData = async() =>{
-try{
-    const response = await fetch(jokeApi);
-    const data = await response.json();
-    console.log(data)
-    // renderJoke(data);
-} catch (error) {
-    console.error('Errore durante il recupero dei dati:', error);
-  }
+const getData = async () => {
+    try {
+        const response = await fetch(jokeApi);
+        const data = await response.json();
+        console.log(data)
+        // renderJoke(data);
+    } catch (error) {
+        console.error('Errore durante il recupero dei dati:', error);
+    }
 }
 
 // Joke rendering function on the page
-const renderJoke = (data) =>{
+const renderJoke = (data) => {
     setup.innerHTML = data.setup;
     punchline.innerHTML = data.punchline;
 }
 
 // Sound playing function on click
-const playSound = () =>{
+const playSound = () => {
     audio.play()
 }
 
